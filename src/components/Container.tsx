87b398033086e911fe88fdf29878713d041ff0ab
Denis <denis2001nik@gmail.com>
2023-01-28 13:40:33 +0300

    Made margin top by the height of status bar

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,28 +1,30 @@
-import React, { ReactElement } from 'react'
-import { StyleSheet, View, ViewProps } from 'react-native'
+import Constants from 'expo-constants';
+import React, { ReactElement } from 'react';
+import { StyleSheet, View, ViewProps } from 'react-native';
 
 export interface ContainerProps extends ViewProps {
-    children?: ReactElement | ReactElement[] | never[]
+	children?: ReactElement | ReactElement[] | never[];
 }
 
 const Container = ({
-    children,
-    style: outerStyle,
-    ...rest
+	children,
+	style: outerStyle,
+	...rest
 }: ContainerProps) => {
-    return (
-        <View style={[styles.container, outerStyle]} {...rest}>
-            {children}
-        </View>
-    )
-}
+	return (
+		<View style={[styles.container, outerStyle]} {...rest}>
+			{children}
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '90%',
-        alignSelf: 'center'
-    }
-})
+	container: {
+		flex: 1,
+		width: '90%',
+		alignSelf: 'center',
+		marginTop: Constants.statusBarHeight,
+	},
+});
 
 export default Container;
